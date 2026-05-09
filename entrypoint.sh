ab6ad8971d3167eadf3bbb8e3ea8efdea3a2f38b
Kristian <kriiiiiiiz@icloud.com>
2026-05-09 19:30:56 -0400

    Refactor placeholder conversion in entrypoint.sh
    Updated the method of converting placeholders in the startup command.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,9 @@ cd /home/container || exit 1
 node --version
 npm --version
 
-MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
+# Convert Pterodactyl placeholders like {{VAR}} into regular shell variables.
+MODIFIED_STARTUP=$(printf '%s' "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 exec /bin/bash -c "${MODIFIED_STARTUP}"
+
