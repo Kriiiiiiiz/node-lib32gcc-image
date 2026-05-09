f690069994c803a921527fa3c011b3267de0d957
Kristian <kriiiiiiiz@icloud.com>
2026-05-09 19:11:29 -0400

    Add entrypoint script
    Create entrypoint.sh to set up the container environment

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /home/container || exit 1
+
+node --version
+npm --version
+
+MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+exec /bin/bash -c "${MODIFIED_STARTUP}"
